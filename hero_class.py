e53074baa343b82230c27ffcd1a89882d81b5db3
KovTon <ikovtonogov@gmail.com>
2023-04-24 21:53:47 +0400

    Hero parameters were added.

---
+++ hero_class.py
@@ -0,0 +1,16 @@
+class Hero():
+    def __init__(self, name: str, character_class: str, race: str, HP: int, AG: int, money_bag: int):
+        self.name = name
+        self.character_class = character_class
+        self.race = race
+        self.HP = HP
+        self.AG = AG
+        self.money_bag = money_bag
+        
+    def movement():
+        pass
+
+
+bard = Hero('Diablo', 'bard', 'undead', 100500, 70, 0)
+
+print(tank.name)
